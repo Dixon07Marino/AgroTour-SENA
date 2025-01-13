facec678606c4831016cdddaaed4dde55a8d5097
José De La Paz <joseantonyodpf@gamil.com>
2025-01-12 23:40:25 -0500

    Restablecimiento

--- docs/index.js
+++ docs/index.js
@@ -15,10 +15,10 @@ app.listen(app.get("port"));
 console.log("Servidor corriendo en puerto",app.get("port"));
 
 //Servido de archivos estaticos
-app.use(express.static(path.join(__dirname, "/pages")));
+app.use(express.static(path.join(__dirname, "/")));
 
 
 
 
 //COnfiguración de las rutas al backend
-app.get("/", (req,res)=> res.sendFile(__dirname + "/pages/index.html"));
+app.get("/", (req,res)=> res.sendFile(__dirname + "/index.html"));
